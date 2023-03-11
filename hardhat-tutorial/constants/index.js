aa902c532fd92ab1830d5cad9ec3884ca3f77ebc
Nithin Chandran P <p.nithinc@gmail.com>
2023-03-11 17:03:48 +0530

    Initial Load

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xf706cd88561BBA72beCd5Bd2Ba8C766610838f73";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
